57656eef582c00937caec566dbd1ee66f5a44f2c
ghil <tussbaum@gmail.com>
2021-07-13 19:23:45 +0300

    Beginning of SmartDecimation version

---
+++ src/ModelBuilder/SmartDecimator.cpp
@@ -0,0 +1,5 @@
+//
+// Created by ghil on 11/07/2021.
+//
+
+#include "SmartDecimator.h"
---
+++ src/ModelBuilder/SmartDecimator.h
@@ -0,0 +1,16 @@
+#ifndef VIVID_SMARTDECIMATOR_H
+#define VIVID_SMARTDECIMATOR_H
+
+namespace vivid
+{
+
+class SmartDecimator {
+
+
+
+
+};
+
+
+} // namespace vivid
+#endif //VIVID_SMARTDECIMATOR_H
